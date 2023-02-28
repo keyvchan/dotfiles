e154780bc267976fe381618006a0b77c6dda4981
Keyv Chan <keyvchan@gmail.com>
2023-02-28 19:50:23 +0800

    fix: notify

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -1,23 +1,14 @@
 local M = {
-  "rcarriga/nvim-notify",
-  -- event = "VeryLazy",
+	"rcarriga/nvim-notify",
 
-  lock = false,
-  -- branch = "feat/disable-animation",
+	lazy = false,
 }
 
 function M.config()
-  require("notify").setup({
-    timeout = 3000,
-    level = vim.log.levels.INFO,
-    fps = 20,
-    max_height = function()
-      return math.floor(vim.o.lines * 0.75)
-    end,
-    max_width = function()
-      return math.floor(vim.o.columns * 0.75)
-    end,
-  })
+	require("notify").setup({
+		background_colour = "#000000",
+	})
+	vim.notify = require("notify")
 end
 
 return M
