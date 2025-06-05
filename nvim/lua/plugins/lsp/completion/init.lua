350a5aa6da9aa7f499e9c6ef607b67d0aab06e84
Keyv Chan <keyvchan@gmail.com>
2025-06-05 10:32:25 +0800

    fix: fix blink sort

--- nvim/lua/plugins/lsp/completion/init.lua
+++ nvim/lua/plugins/lsp/completion/init.lua
@@ -1,84 +1,77 @@
 local M = {
-    "saghen/blink.cmp",
-    event = { "InsertEnter", "CmdlineEnter" },
+	"saghen/blink.cmp",
+	event = { "InsertEnter", "CmdlineEnter" },
 
-    version = "*",
+	version = "*",
 
-    opts = {
-        completion = {
-            list = {
-                selection = {
-                    preselect = false,
-                    auto_insert = true,
-                },
-            },
-            accept = {
-                auto_brackets = {
-                    enabled = false,
-                },
-            },
-            menu = {
-                border = "none",
-            },
-            documentation = {
-                auto_show = true,
-                auto_show_delay_ms = 100,
-            },
-        },
-        fuzzy = {
-            sorts = {
-                "exact",
-                "sort_text",
-                "score"
-            }
-        },
-        keymap = {
-            preset = "enter",
-            ["<Tab>"] = { "select_next", "fallback" },
-            ["<S-Tab>"] = { "select_prev", "fallback" },
-            ["<CR>"] = { "fallback" },
-            ["<C-e>"] = { "fallback" },
-        },
+	opts = {
+		completion = {
+			list = {
+				selection = {
+					preselect = false,
+					auto_insert = true,
+				},
+			},
+			accept = {
+				auto_brackets = {
+					enabled = false,
+				},
+			},
+			menu = {
+				border = "none",
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 100,
+			},
+		},
+		keymap = {
+			preset = "enter",
+			["<Tab>"] = { "select_next", "fallback" },
+			["<S-Tab>"] = { "select_prev", "fallback" },
+			["<CR>"] = { "fallback" },
+			["<C-e>"] = { "fallback" },
+		},
 
-        appearance = {
-            use_nvim_cmp_as_default = true,
-            nerd_font_variant = "mono",
-        },
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono",
+		},
 
-        sources = {
-            default = { "lsp", "path", "buffer" },
-            providers = {
-                lsp = {
-                    fallbacks = {},
-                },
-                path = {
-                    opts = {
-                        trailing_slash = true,
-                        label_trailing_slash = true,
-                    },
-                },
-            },
-        },
-        signature = { enabled = true },
-        cmdline = {
-            keymap = {
-                preset = "enter",
-                ["<Tab>"] = { "select_next", "fallback" },
-                ["<S-Tab>"] = { "select_prev", "fallback" },
-                ["CR"] = { "fallback" },
-            },
-            completion = {
-                list = {
-                    selection = {
-                        preselect = false,
-                        auto_insert = true,
-                    },
-                },
-                menu = { auto_show = true },
-            },
-        },
-    },
-    opts_extend = { "sources.default" },
+		sources = {
+			default = { "lsp", "path", "buffer" },
+			providers = {
+				lsp = {
+					fallbacks = {},
+				},
+				path = {
+					opts = {
+						trailing_slash = true,
+						label_trailing_slash = true,
+					},
+				},
+			},
+		},
+		signature = { enabled = true },
+		cmdline = {
+			keymap = {
+				preset = "enter",
+				["<Tab>"] = { "select_next", "fallback" },
+				["<S-Tab>"] = { "select_prev", "fallback" },
+				["CR"] = { "fallback" },
+			},
+			completion = {
+				list = {
+					selection = {
+						preselect = false,
+						auto_insert = true,
+					},
+				},
+				menu = { auto_show = true },
+			},
+		},
+	},
+	opts_extend = { "sources.default" },
 }
 
 return M
