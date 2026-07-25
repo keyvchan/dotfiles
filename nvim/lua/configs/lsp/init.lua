a73bb89f7af74c12f8886c0e721d9a711f8f217f
chenkeyv <keyvchen@gmail.com>
2026-07-25 08:52:37 +0800

    feat(nvim): enable supported LSP features globally

--- nvim/lua/configs/lsp/init.lua
+++ nvim/lua/configs/lsp/init.lua
@@ -1,27 +1,10 @@
+vim.lsp.codelens.enable()
+vim.lsp.document_color.enable()
 vim.lsp.inlay_hint.enable()
-
-local on_type_formatters = {
-	rust_analyzer = true,
-}
-
-local function configure_on_type_formatting(client)
-	if on_type_formatters[client.name] then
-		vim.lsp.on_type_formatting.enable(true, { client_id = client.id })
-	end
-end
-
-vim.lsp.on_type_formatting.enable(false)
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("DotfilesLspFormatting", { clear = true }),
-	callback = function(event)
-		local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
-		configure_on_type_formatting(client)
-	end,
-})
-
-for _, client in ipairs(vim.lsp.get_clients()) do
-	configure_on_type_formatting(client)
-end
+vim.lsp.inline_completion.enable()
+vim.lsp.linked_editing_range.enable()
+vim.lsp.on_type_formatting.enable()
+vim.lsp.semantic_tokens.enable()
 
 require("configs.lsp.server")
 require("configs.lsp.keymap")
