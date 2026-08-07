a364a0deb2d03e73a29fe776a111cfd6567b2d15
chenkeyv <keyvchen@gmail.com>
2026-08-07 23:28:41 +0800

    feat(nvim): add common language tooling

--- nvim/lua/configs/lsp/server/init.lua
+++ nvim/lua/configs/lsp/server/init.lua
@@ -6,6 +6,29 @@ vim.lsp.config("sourcekit", {
 	filetypes = { "swift" },
 })
 
-vim.lsp.enable({ "gopls", "clangd", "lua_ls", "rust_analyzer", "sourcekit", "ty" })
+vim.lsp.config("ruff", {
+	on_attach = function(client)
+		-- Keep ty as the Python hover and type-information provider.
+		client.server_capabilities.hoverProvider = false
+	end,
+})
+
+vim.lsp.enable({
+	"bashls",
+	"clangd",
+	"cssls",
+	"gopls",
+	"html",
+	"jsonls",
+	"lua_ls",
+	"marksman",
+	"ruff",
+	"rust_analyzer",
+	"sourcekit",
+	"taplo",
+	"tsgo",
+	"ty",
+	"yamlls",
+})
 
 require("configs.lsp.server.lua_ls")
--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,6 +1,13 @@
 require("conform").setup({
 	formatters_by_ft = {
+		c = { "clang-format" },
+		cpp = { "clang-format" },
+		go = { "goimports", "gofmt" },
 		lua = { "stylua" },
+		python = { "ruff_format" },
+		rust = { "rustfmt" },
+		sh = { "shfmt" },
+		swift = { "swift" },
 	},
 	format_on_save = {
 		timeout_ms = 500,
